42abaa4510c693b9b218933b4bd3e732aca8e51d
yook <si89336@naver.com>
2024-09-27 12:37:15 +0900

    💄 add MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,15 @@
+import OneDaySection from '../components/layout/OneDaySection';
 import Header from '../components/layout/Header';
 import MainSection from '../components/layout/MainSection';
+import GraphSection from '../components/layout/GraphSection';
 
 function MainPage() {
     return (
         <>
             <Header />
             <MainSection />
+            <OneDaySection />
+            <GraphSection />
         </>
     );
 }
