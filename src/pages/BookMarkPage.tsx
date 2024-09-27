5302f7c2ed61d3fa252a6dab8e4dd3fabc56044c
yook <si89336@naver.com>
2024-09-27 17:52:28 +0900

    💄 update BookMarkPage

--- src/pages/BookMarkPage.tsx
+++ src/pages/BookMarkPage.tsx
@@ -2,21 +2,39 @@ import CardList from '../components/common/CardList';
 import Header from '../components/layout/Header';
 import styled from 'styled-components';
 import GlobalStyle from '../styles/GlobalStyle';
+import illust from '../assets/illust.png';
+import { useState } from 'react';
 
 function BookMarkPage() {
+    const [length, setLength] = useState(0);
+
     return (
         <>
             <GlobalStyle />
             <Header />
             <Container className="mw">
-                <Title>친구들에게 관심을 가져주셔서 감사해요!</Title>
-                <CardList paddingTop="2rem" />
+                {length ? (
+                    <>
+                        <Title>친구들에게 관심을 가져주셔서 감사해요!</Title>
+                        <CardList paddingTop="2rem" />
+                    </>
+                ) : (
+                    <IllustContainer>
+                        <IllustArea>
+                            <Illust src={illust} alt="dog and cat" />
+                        </IllustArea>
+                        <SubTitle>다시 보고 싶은 친구들을 추가해주세요!</SubTitle>
+                    </IllustContainer>
+                )}
             </Container>
         </>
     );
 }
 
-const Container = styled.section``;
+const Container = styled.section`
+    padding-bottom: 8rem;
+    position: relative;
+`;
 
 const Title = styled.h3`
     font-size: 2rem;
@@ -27,6 +45,41 @@ const Title = styled.h3`
     @media (max-width: 650px) {
         font-size: 1.7rem;
     }
+`;
+
+const SubTitle = styled.h3`
+    font-size: 2rem;
+    font-weight: 900;
+    letter-spacing: -2px;
+
+    @media (max-width: 650px) {
+        font-size: 1.7rem;
+    }
+`;
+
+const IllustContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 5rem;
+
+    @media (max-width: 650px) {
+        position: absolute;
+    }
+`;
+
+const IllustArea = styled.div`
+    width: 40%;
+
+    @media (max-width: 650px) {
+        width: 100%;
+    }
+`;
+
+const Illust = styled.img`
+    width: 100%;
+    object-fit: contain;
 `;
 
 export default BookMarkPage;
