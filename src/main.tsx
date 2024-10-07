9fe1baf48e537cd21b0148baa5176ef59db3d917
yook <si89336@naver.com>
2024-10-07 09:48:15 +0900

    ✨ set react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { store } from './store/store.ts';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </StrictMode>
 );
