0486855c36bf5bdde97c695e48aef68167c0b77c
yook <si89336@naver.com>
2024-10-07 10:22:05 +0900

    ⚡️ improve positions

--- src/components/features/ShelterMap.tsx
+++ src/components/features/ShelterMap.tsx
@@ -18,12 +18,20 @@ function ShelterMap({ setSelectedShelter }: ShelterMapProps) {
             latlng: new kakao.maps.LatLng(37.8459543, 127.4991358),
         },
         {
+            title: '부천시수의사회',
+            latlng: new kakao.maps.LatLng(37.4887382052, 126.7658249447),
+        },
+        {
             title: '고양시동물보호센터',
             latlng: new kakao.maps.LatLng(37.6496069, 126.870066),
         },
         {
             title: '광주TNR동물병원송정',
             latlng: new kakao.maps.LatLng(37.41746097, 127.2752964),
+        },
+        {
+            title: '광주TNR동물병원초월',
+            latlng: new kakao.maps.LatLng(37.4158943728, 127.2761647488),
         },
         {
             title: '수원시 동물보호센터',
@@ -36,6 +44,10 @@ function ShelterMap({ setSelectedShelter }: ShelterMapProps) {
         {
             title: '가나동물병원',
             latlng: new kakao.maps.LatLng(37.48350736, 126.7631747),
+        },
+        {
+            title: '정샘동물병원',
+            latlng: new kakao.maps.LatLng(37.5015303684, 126.769059806),
         },
         {
             title: '24시아이동물메디컬',
@@ -88,6 +100,22 @@ function ShelterMap({ setSelectedShelter }: ShelterMapProps) {
         {
             title: '남양동물보호센터',
             latlng: new kakao.maps.LatLng(37.22494992, 126.8434243),
+        },
+        {
+            title: '구리반려동물문화센터',
+            latlng: new kakao.maps.LatLng(37.6131390076, 127.1409110928),
+        },
+        {
+            title: '광명반함센터',
+            latlng: new kakao.maps.LatLng(37.4743689676, 126.8684627314),
+        },
+        {
+            title: '안성시농업기술센터',
+            latlng: new kakao.maps.LatLng(37.0234771716, 127.2868211181),
+        },
+        {
+            title: '의왕시청',
+            latlng: new kakao.maps.LatLng(37.3521649674, 126.9822974283),
         },
     ];
 
